33e5194f9fe821cae268bf35279d8b08fbc963e1
РоманчиковаС <@DESKTOP-VMEIG62>
2019-09-30 21:07:26 +0600

    Добавьте файлы проекта.

---
+++ QuestRoomCatalog.BusinessLayer/Helpers/QuestLogosHelper.cs
@@ -0,0 +1,32 @@
+﻿using QuestRoomCatalog.BusinessLayer.BusinessObjects;
+using QuestRoomCatalog.DataLayer;
+using QuestRoomCatalog.DataLayer.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomCatalog.BusinessLayer.Helpers
+{
+    public class QuestLogosHelper
+    {
+        UnitOfWork<QuestsLogos> Db { get; set; }
+
+        public QuestLogosHelper(UnitOfWork<QuestsLogos> uow)
+        {
+            Db = uow;
+        }
+
+        public void Create(QuestsLogosBO Bo)
+        {
+            if(Bo.Id == 0)
+            {
+                QuestsLogos questLogos = AutoMapper<QuestsLogosBO, QuestsLogos>.Map(Bo);
+                Db.GenericUowRepository.Add(questLogos);
+                Db.Save();
+            }
+
+        }
+    }
+}
---
+++ QuestRoomCatalog.DataLayer/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataLayer.Repositories;
+using QuestRoomCatalog.DataLayer;
+
+namespace QuestRoomCatalog.DataLayer.UnitOfWork
+{
+    public class UnitOfWork<T> : IDisposable, IUnitOfWork<T> where T: class
+    {
+        private Model1 db;
+        private bool disposed = false;
+        Repository<T> _genericRepository;
+
+        public UnitOfWork()
+        {
+            db = new Model1();
+        }
+
+
+        //public Repository<Authors> AuthorUowRepository
+        //{
+        //    get
+        //    {
+
+        //        if (this._authorUowRepository == null)
+        //            _authorUowRepository = new Repository<Authors>(db);
+        //        return _authorUowRepository;
+        //    }
+        //}
+
+        //public Repository<Books> BookUowRepository
+        //{
+        //    get
+        //    {
+        //        if (this._bookUowRepository == null)
+        //            _bookUowRepository = new Repository<Books>(db);
+        //        return _bookUowRepository;
+        //    }
+        //}
+
+        //public Repository<User> UserUowRepository
+        //{
+        //    get
+        //    {
+        //        if (this._userUowRepository == null)
+        //            _userUowRepository = new Repository<User>(db);
+        //        return _userUowRepository;
+        //    }
+        //}
+
+        //public Repository<Rent> RentUowRepository
+        //{
+        //    get
+        //    {
+                
+        //        if (this._rentUowRepository == null)
+        //            _rentUowRepository = new Repository<Rent>(db);
+        //        return _rentUowRepository;
+        //    }
+        //}
+
+        //public Repository<Genre> GenreUowRepository
+        //{
+        //    get
+        //    {
+
+        //        if (this._genreUowRepository == null)
+        //            _genreUowRepository = new Repository<Genre>(db);
+        //        return _genreUowRepository;
+        //    }
+        //}
+
+        public Repository<T> GenericUowRepository
+        {
+            get
+            {
+                if (this._genericRepository == null)
+                    _genericRepository = new Repository<T>(db);
+                return _genericRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
